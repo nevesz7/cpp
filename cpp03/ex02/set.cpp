8f0ee7f439de363707ca340e3282e382a72ebc0f
Nevesz7 <raphael.nevesz@live.com>
2024-02-12 20:08:22 -0300

    fixing set.cpp

--- cpp03/ex02/set.cpp
+++ cpp03/ex02/set.cpp
@@ -5,22 +5,26 @@ void	ClapTrap::setName(std::string name)
 	this->name = name;
 }
 
-void	ClapTrap::setAttackDamage(unsigned int amount)
+//if amount is smaller than 0, attack is set to 0
+void	ClapTrap::setAttackDamage(int amount)
 {
-	this->attackDamage = amount;
+	this->attackDamage = (amount < 0) ? 0 : amount;
 }
 
-void	ClapTrap::setHitPoints(unsigned int amount)
+//if amount is smaller than 0, HP is set to 0
+void	ClapTrap::setHitPoints(int amount)
 {
-	this->hitPoints = amount;
+	this->hitPoints = (amount < 0) ? 0 : amount;
 }
 
-void	ClapTrap::setEnergyPoints(unsigned int amount)
+//if amount is smaller than 0, energy is set to 0
+void	ClapTrap::setEnergyPoints(int amount)
 {
-	this->energyPoints = amount;
+	this->energyPoints = (amount < 0) ? 0 : amount;
 }
 
-void ClapTrap::setMaxHitPoints(unsigned int amount)
+//if amount is smaller than 0, maxhp is set to 0
+void ClapTrap::setMaxHitPoints(int amount)
 {
-	this->maxHitPoints = amount;
+	this->maxHitPoints = (amount < 1) ? 1 : amount;
 }
